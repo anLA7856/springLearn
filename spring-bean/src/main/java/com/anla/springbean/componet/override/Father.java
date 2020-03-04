299712da4af6cec931b16dfb87a0ad12b5506ff4
luoan <luoan007@gmail.com>
2020-03-04 16:09:03 +0800

    增加 Father

---
+++ spring-bean/src/main/java/com/anla/springbean/componet/override/Father.java
@@ -0,0 +1,13 @@
+package com.anla.springbean.componet.override;
+
+/**
+ * @author luoan
+ * @version 1.0
+ * @date 2020/3/4 13:59
+ **/
+public class Father {
+
+    public void fatherSay(){
+        System.out.println("I am father");
+    }
+}

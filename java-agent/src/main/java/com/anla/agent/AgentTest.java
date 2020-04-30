e5a570498d6cf27e9d427a3059cb435fb75175f6
luoan <350493391@qq.com>
2020-04-30 22:16:54 +0800

    增加 agent 例子

--- java-agent/src/main/java/com/anla/agent/AgentTest.java
+++ java-agent/src/main/java/com/anla/agent/AgentTest.java
@@ -1,6 +1,7 @@
 package com.anla.agent;
 
 /**1. 加上打包完的agent：-javaagent:E:\idea-project\anla\springLearn\java-agent\target\java-agent-1.0-SNAPSHOT.jar
+ * 2. agent 详解：https://blog.csdn.net/scorpio3k/article/details/6745443
  * @author luoan
  * @version 1.0
  * @date 2020/4/27 11:02

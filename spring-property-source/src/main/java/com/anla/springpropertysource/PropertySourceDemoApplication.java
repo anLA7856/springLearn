b7832f366b554b63916ce75b3c0149a7beb531ae
anLA7856 <luoan007@gmail.com>
2020-11-02 23:30:29 +0800

    some PropertySource

--- spring-property-source/src/main/java/com/anla/springpropertysource/PropertySourceDemoApplication.java
+++ spring-property-source/src/main/java/com/anla/springpropertysource/PropertySourceDemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 答案是会
+ * 定义多个同名配置，会覆盖吗？答案是不一定，看具体逻辑，Spring 中PropertySource有多个，有优先级的，所以可能是获取到了优先级更高配置类中的配置。但是使用 `@Value` 注解是，会被覆盖
+ *
  * @author luoan
  * @version 1.0
  * @date 2020/10/30 13:43
